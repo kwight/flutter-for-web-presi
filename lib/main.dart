a30517e8e6cb4d1879d71df5396c644a0b2b54d3
Kirk Wight <kwight@kwight.ca>
2019-06-11 08:05:01 -0700

    Move the theme to its own file.

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,9 @@
 import 'package:flutter_web/cupertino.dart';
 import 'package:meetup_2/util/router.dart';
+import 'package:meetup_2/util/theme.dart';
 
 void main() => runApp(CupertinoApp(
       debugShowCheckedModeBanner: false,
       onGenerateRoute: router,
-      theme: CupertinoThemeData(
-        barBackgroundColor: Color.fromRGBO(5, 90, 157, 1),
-        primaryColor: Color.fromRGBO(47, 185, 246, 1),
-        textTheme: CupertinoTextThemeData(
-          navTitleTextStyle: TextStyle(
-            fontSize: 20,
-            color: Color.fromRGBO(255, 255, 255, 1),
-          ),
-          navLargeTitleTextStyle: TextStyle(
-            color: Color.fromRGBO(74, 74, 74, 1),
-            fontSize: 60,
-          ),
-          navActionTextStyle: TextStyle(
-            fontSize: 100,
-          ),
-          tabLabelTextStyle: TextStyle(
-            color: Color.fromRGBO(200, 200, 200, 1),
-            fontSize: 20,
-            height: 1.3,
-          ),
-          textStyle: TextStyle(
-            color: Color.fromRGBO(110, 114, 116, 1),
-            fontSize: 30,
-            height: 2,
-          ),
-        ),
-      ),
+      theme: themeData(),
     ));
